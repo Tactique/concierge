bb71e32919697f44ebf3e92833759c0b0a70c2b2
Quinten Palmer <quintenpalmer@gmail.com>
2014-05-06 11:09:12 -0400

    Concierge can no clone

--- concierge.py
+++ concierge.py
@@ -11,6 +11,20 @@ except ImportError:
 file_name = __file__
 file_dir = os.path.dirname(os.path.realpath(file_name))
 
+
+def check_output(*popenargs, **kwargs):
+    if 'stdout' in kwargs:
+        raise ValueError('stdout argument not allowed, it will be overridden.')
+    process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+    output, unused_err = process.communicate()
+    retcode = process.poll()
+    if retcode:
+        cmd = kwargs.get("args")
+        if cmd is None:
+            cmd = popenargs[0]
+        raise subprocess.CalledProcessError(retcode, cmd, output=output)
+    return output
+
 class Concierge:
     def __init__(self, config_file):
         config = configparser.SafeConfigParser()
@@ -25,8 +39,30 @@ class Concierge:
         self.remote = config.get(git_section, 'remote')
         self.organization = config.get(git_section, 'organization')
 
+    def clone_all(self):
+        for repo in self.repos:
+            full_repo_description = get_full_repo_description(
+                self.protocol, self.remote, self.organization, repo)
+            output = check_output(['git', 'clone', full_repo_description])
+            print(repo)
+            print(output)
+
+
+def get_full_repo_description(protocol, remote, organization, repo):
+    protocol_info = {
+        'git': ('@', ':'),
+        'https': ('://', '/'),
+    }
+    try:
+        protocol_delimiter, repo_delimiter = protocol_info[protocol]
+    except KeyError:
+        raise Exception('Unknown protocol: %s, must be one of %s' % (
+            protocol, ' '.join(protocol_info.keys())))
+    return protocol + protocol_delimiter + remote + repo_delimiter + organization + '/' + repo
+
 
 commands = {
+    'clone': 'clone_all',
 }
 
 
